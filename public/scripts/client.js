fd141e5e06b223751d94ee043047c4cb9a250d2f
Ammar Shakir <shakirammar12@gmail.com>
2020-09-10 22:13:17 +0000

    cleaned up the code and fixed linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $('#error').hide();
   $('.new-tweet').hide();
 
+
+ // making text input safe to post
+
   const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -9,21 +12,23 @@ $(document).ready(function() {
   }
 
   const createTweetElement = (tweet) => {
-
-    const $tweet = $(`<article class= "tweet"> 
-    <header class="random-tweeter">
-        <img src=${escape(tweet.user.avatars)}>
-        <div class="handle">
-          <span> ${escape(tweet.user.name)}</span> 
-          <span class="tag">${escape(tweet.user.handle)}</span>
-        </div>
-    </header>
-    <p class="existing-text">${escape(tweet.content.text)}</p>
-    <hr>
-    <footer>${moment(tweet.created_at).fromNow()} <span class="symbols"> &#127988 	&#128257 	&#128153</span></footer>
-  </article>`);
-  return $tweet;
+    const $tweet = $(
+    `<article class= "tweet"> 
+      <header class="random-tweeter">
+          <img src=${escape(tweet.user.avatars)}>
+          <div class="handle">
+            <span> ${escape(tweet.user.name)}</span> 
+            <span class="tag">${escape(tweet.user.handle)}</span>
+          </div>
+      </header>
+      <p class="existing-text">${escape(tweet.content.text)}</p>
+      <hr>
+      <footer>${moment(tweet.created_at).fromNow()} <span class="symbols"> &#127988 	&#128257 	&#128153</span></footer>
+    </article>`);
+    return $tweet;
   };
+
+  // Adding tweets to the article tag
 
   const renderTweets = function(tweets) {
     for (let $tweet of tweets) {
@@ -32,18 +37,26 @@ $(document).ready(function() {
     }
   };
 
+  // writing a new tweet: toggle
+  
   $("#toggle").on('click', () => {
     $(".new-tweet").slideToggle(1000);
     $("#tweet-text").focus();
   })
-
+  
+  // get request from /tweets
+  
   const loadTweets = () => {
     $.get('/tweets', (tweet) => {
-        renderTweets(tweet);
-      })
+      renderTweets(tweet);
+    })
   };
+  
+  // loading all tweets when site starts 
 
   loadTweets();
+
+  // post request to /tweets
   
   const $postTweet = $('#post-tweet');
   $postTweet.on('submit', function (event) {
